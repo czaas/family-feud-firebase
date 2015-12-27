e81e6f8d88f8b310a39cac12525f237274e7bf2d
Cameron Zaas <cameronzaas@gmail.com>
2015-12-27 11:04:24 -0800

    When host page makes an update, audience page updates.

--- app/components/audience-page.js
+++ app/components/audience-page.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import Rebase from 're-base';
+
+let base = Rebase.createClass('https://family-feud-v2.firebaseio.com');
 
 export class Audience extends React.Component {
+
+	constructor(){
+		super();
+
+		this.state = {
+			game: []
+		};
+	}
+
+	componentDidMount(){
+		this.ref = base.syncState( 'games/test', {
+			context: this,
+			state: 'game',
+			asArray: true,
+		});
+	}
+
+	componentWillUnmount(){
+		base.removeBinding(this.ref);
+	}
+
+
 	render() {
+
+		let currentQuestion = this.state.game.map((q) => {
+			return (q.currentQuestion) ? 
+				<span>{q.q}</span> :
+				null;
+		});
+
 		return (
 			<div>
 				<h2>Audience</h2>
+				<p>{currentQuestion || 'No currentQuestion'}</p>
 			</div>
 		);
 	}
